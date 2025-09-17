c517379fea4c803c5cba668871ed5119ef8893e5
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-17 13:15:22 +0200

    aggiunta img "pin" con absolute

---
+++ script.js
@@ -0,0 +1,7 @@
+/* selezione  variabili di endpoint*/
+const apiRef = "https://lanciweb.github.io/demo/api/pictures/"
+
+/* selezione elementi di Output*/
+ const data = document.querySelector(".dataCard")
+ const titolo = document.querySelector(".dataCard")
+ const img = document.querySelector(".imgCard")
