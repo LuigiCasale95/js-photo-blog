d952c43b59aa698b8937f79c416e8bb9dfb68f7b
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-18 11:55:54 +0200

    aggiunta base per hoverlay img e @media per il responsive di essa

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* selezione elementi di Output*/
-const data = document.querySelector(".dataCard")
+/* const data = document.querySelector(".dataCard")
 const titolo = document.querySelector(".dataCard")
-const img = document.querySelector(".imgCard")
+const img = document.querySelector(".imgCard") */
 const rowOutput = document.querySelector(".row")
 
 /* selezione  variabili di endpoint*/
@@ -20,8 +20,10 @@ const apiRef = "https://lanciweb.github.io/demo/api/pictures/";
         /*  ciclo per prendere ogni elemento necessario */
         elementi.forEach(elemento => {
 //            console.log(elemento);
+
             accumuloCard += `
             <div class="cardContainer position-relative m-4 col-sm-12 col-md-6 col-lg-4">
+            <a href="">
                     <img class="imgPin position-absolute start-50 translate-middle" src="img/pin.svg" alt="crok">
 
                     <div class="boxImg">
@@ -32,10 +34,22 @@ const apiRef = "https://lanciweb.github.io/demo/api/pictures/";
                         <span class="dataCard">${elemento.date}</span>
                         <h2 class="titoloCard">${elemento.title}</h2>
                     </section>
+                    </a>
                 </div>
             `
           rowOutput.innerHTML = accumuloCard
         });
+
+
+        /* Selezione delle card */
+        const cards = document.querySelectorAll(".cardContainer") 
+        console.log(cards);
+
+        cards.addEventListener("click",
+             function() {
+            console.log("ciao")
+
+        })
         
         
     }) 
